b3956cdf0a176d3457bf9afce9df682e264e6487
danielz0102 <danyzglez@hotmail.com>
2024-12-16 15:55:37 -0600

    Create gameController object

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const Gameboard = (() => {
   const board = []
 
-  for (let i = 0; i < 3; i++) {
-    board[i] = []
-    for (let j = 0; j < 3; j++) {
-      board[i].push(' ')
+  const initBoard = () => {
+    for (let i = 0; i < 3; i++) {
+      board[i] = []
+      for (let j = 0; j < 3; j++) {
+        board[i].push(null)
+      }
     }
   }
-
-  const getBoard = () => board
 
   const printBoard = () => {
     for (let i = 0; i < 3; i++) {
@@ -16,8 +16,47 @@ const Gameboard = (() => {
     }
   }
 
-  return { getBoard, printBoard }
+  const setBoard = (mark, row, col) => {
+    if (!board[row][col]) {
+      board[row][col] = mark
+    }
+  }
+
+  initBoard()
+
+  return { printBoard, initBoard, setBoard }
 })()
 
-const board = Gameboard.getBoard()
-Gameboard.printBoard()
+function gameController(playerName1, playerName2) {
+  const players = [
+    {
+      name: playerName1,
+      mark: 'x',
+      turn: true
+    },
+    {
+      name: playerName2,
+      mark: 'o',
+      turn: false
+    }
+  ]
+
+  const getActivePlayer = () => players.find(player => player.turn === true)
+
+  const switchTurn = () => {
+    players[0].turn ^= true
+    players[1].turn ^= true
+  }
+
+  const playRound = () => {
+    const activePlayer = getActivePlayer()
+    console.log(`${activePlayer.name}'s turn`)
+    Gameboard.printBoard()
+  }
+
+  playRound()
+
+  return { playRound }
+}
+
+const game = gameController('daniel', 'sara')
